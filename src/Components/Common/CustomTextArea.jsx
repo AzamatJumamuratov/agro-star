0a5470cd35d21147c0b4822d6cc1fab31157cdc7
Azamat <jumamuratovazamat2004@gmail.com>
2025-06-12 23:32:04 +0500

    changing form layouts

--- src/Components/Common/CustomTextArea.jsx
+++ src/Components/Common/CustomTextArea.jsx
@@ -5,7 +5,7 @@ const CustomTextArea = ({ name, id, required, placeholder }) => {
       id={id}
       required={required}
       placeholder={placeholder}
-      className="w-full lg:min-h-96 md:min-h-64 min-h-48 mb-8 border-2 border-[#F1F1F1] bg-white rounded-lg lg:rounded-2xl xl:placeholder:text-2xl xl:text-2xl lg:text-base lg:placeholder:text-base text-sm xl:py-6 lg:py-4 py-2 xl:px-9 lg:px-6 px-2"
+      className="w-full xl:min-h-96 lg:min-h-72 md:min-h-64 min-h-48 mb-8 border-2 border-[#F1F1F1] bg-white rounded-lg lg:rounded-2xl xl:placeholder:text-2xl  lg:placeholder:text-sm placeholder:text-xs xl:text-2xl lg:text-sm text-xs xl:py-6 lg:py-4 py-2 xl:px-9 lg:px-6 px-2"
     />
   );
 };
--- src/Components/Common/FormInput.jsx
+++ src/Components/Common/FormInput.jsx
@@ -7,7 +7,7 @@ const FormInput = ({ name, id, type, autoComplete, placeholder, required }) => {
       autoComplete={autoComplete}
       placeholder={placeholder}
       required={required}
-      className="mb-6 w-full border-2 border-[#F1F1F1] bg-white rounded-lg lg:rounded-2xl xl:placeholder:text-2xl xl:text-2xl lg:text-base lg:placeholder:text-base text-sm xl:py-6 lg:py-4 py-2 xl:px-9 lg:px-6 px-2"
+      className="xl:mb-6 lg:mb-4 mb-2 w-full border-2 border-[#F1F1F1] bg-white rounded-lg lg:rounded-2xl xl:placeholder:text-2xl lg:placeholder:text-sm placeholder:text-xs xl:text-2xl lg:text-sm text-sm   xl:py-6 lg:py-3 py-2 xl:px-9 lg:px-6 px-2"
     />
   );
 };
