fbbffc074d8e8a47df5e71ee1e6289dca5b8d7fe
Azamat <jumamuratovazamat2004@gmail.com>
2025-06-12 18:41:19 +0500

    Projects action and loader update

--- src/Data Fetching/ProjectsData.js
+++ src/Data Fetching/ProjectsData.js
@@ -1,37 +1,49 @@
-import project_image from "../assets/images/project.png";
+// import project_image from "../assets/images/project.png";
 import ConvertToJSonFormData from "../Utils/FromDataToJson";
 import FetchData from "./FetchData";
 
 export async function loader() {
-  // let response = await FetchData("company-info", {
-  //   headers: {
-  //     "Accept-Language": "ru",
-  //   },
-  // });
-  // if (response.ok) {
-  //   return { success: true };
-  // } else {
-  //   console.log("Eror Code : " + response.status + "Text : " + response.status);
-  // }
+  let response = await FetchData("projects", {
+    headers: {
+      "Accept-Language": "ru",
+    },
+  });
+  if (response.ok) {
+    return await response.json();
+  } else {
+    console.Log("Eror Code : " + response.status + "Text : " + response.status);
+  }
 
-  return {
-    results: [
-      {
-        id: "1",
-        title: "Органическое земледелие",
-        description:
-          "Развитие экологически чистых методов выращивания сельскохозяйственных культур.",
-        image: project_image,
-      },
-    ],
-  };
+  // return {
+  //   results: [
+  //     {
+  //       id: "1",
+  //       title: "Органическое земледелие",
+  //       description:
+  //         "Развитие экологически чистых методов выращивания сельскохозяйственных культур.",
+  //       image: project_image,
+  //     },
+  //   ],
+  // };
 }
 
 export async function action({ request }) {
   const formData = await request.formData();
+  const dataJson = ConvertToJSonFormData(formData);
 
-  const json = ConvertToJSonFormData(formData);
+  let response = await FetchData("project-comments/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: dataJson,
+  });
+  if (response.ok) {
+    return await response.json();
+  } else {
+    console.Log("Eror Code : " + response.status + "Text : " + response.status);
+  }
 
-  // Redirect or return a response as needed
-  return { success: true };
+  // // Redirect or return a response as needed
+  // return { success: true };
 }
