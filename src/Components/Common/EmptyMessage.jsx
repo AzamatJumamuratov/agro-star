570b6ef271a268faa21b8d2f047a09ca2a4f3ab2
Azamat <jumamuratovazamat2004@gmail.com>
2025-06-24 21:15:33 +0500

    handling errors in empty message.jsx

--- src/Components/Common/EmptyMessage.jsx
+++ src/Components/Common/EmptyMessage.jsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 
 const EmptyMessage = () => {
-  const { t } = useTranslation;
+  const { t } = useTranslation();
   return (
     <p className="xl:text-lg lg:text-sm text-xs col-span-full text-gray-500">
       {t("empty")}
