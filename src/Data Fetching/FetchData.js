134135eaaaae0e7186c8183888c0dd669a4bd47f
Azamat <jumamuratovazamat2004@gmail.com>
2025-07-01 16:29:31 +0500

    changing server api

--- src/Data Fetching/FetchData.js
+++ src/Data Fetching/FetchData.js
@@ -1,4 +1,4 @@
-const base_url = "https://agro.pythonanywhere.com/api/";
+const base_url = "https://server.beruniy-agrostar.uz/api/";
 
 export default async function FetchData(path, options) {
   return fetch(base_url + path, options);
