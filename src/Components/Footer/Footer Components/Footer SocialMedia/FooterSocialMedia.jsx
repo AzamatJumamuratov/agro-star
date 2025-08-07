609cc1ef9eb3fbb84f0dcb8d3bddf45562ebf6bd
Azamat <jumamuratovazamat2004@gmail.com>
2025-08-07 13:06:30 +0500

    changin footer logo

--- src/Components/Footer/Footer Components/Footer SocialMedia/FooterSocialMedia.jsx
+++ src/Components/Footer/Footer Components/Footer SocialMedia/FooterSocialMedia.jsx
@@ -1,19 +1,18 @@
-import logo from "../../../../assets/logo.svg";
-import instagramIcon from "./instagram_icon.svg";
+import logo from "../../../../assets/Beruniy - Agrostar.png";
+// import instagramIcon from "./instagram_icon.svg";
 import telegramIcon from "./telegram_icon.svg";
 import facebookIcon from "./facebook_icon.svg";
 import linkedinIcon from "./linkedin_icon.svg";
 import { Link, useLoaderData } from "react-router";
+import Logo from "../../../Header/HeaderTop/Logo/Logo";
 
 const FooterSocialMedia = () => {
   const social_links = useLoaderData().social_links;
   return (
     <div className="max-md:mb-6">
       <div className="flex items-center mb-6 xl:mb-12 gap-4">
-        <img src={logo} className="xl:h-auto h-8" />
-        <h3 className="xl:text-2xl text-lg text-white font-bold">
-          Beruniy - Agro Star
-        </h3>
+        {/* <img src={logo} className="xl:h-11 lg:h-9  h-8" /> */}
+        <Logo />
       </div>
       <div className="flex gap-6">
         <Link to={social_links.telegram} type="_blank" className="">
--- src/Components/Header/HeaderTop/Logo/Logo.jsx
+++ src/Components/Header/HeaderTop/Logo/Logo.jsx
@@ -4,13 +4,9 @@ import LogoIcon from "../../../../assets/Beruniy - Agrostar.png";
 const Logo = () => {
   return (
     <div className="flex items-center gap-3  h-auto lg:w-auto  md:justify-center">
-      <img src={LogoIcon} alt="Logo" className="xl:h-11 lg:h-9  h-8" />
-      {/* <Link
-        to="/"
-        className="2xl:text-[28px] xl:text-xl lg:text-lg  text-white font-bold"
-      >
-        Beruniy - Agro Star
-      </Link> */}
+      <Link to={"/"}>
+        <img src={LogoIcon} alt="Logo" className="xl:h-11 lg:h-9  h-8" />
+      </Link>
     </div>
   );
 };
